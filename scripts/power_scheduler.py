bb38db2e6ff25c2adf35d22dbd6974a9c5b797ae
Jussi Ohenoja <juice@swagman.org>
2023-03-04 12:43:47 +0200

    Add skeleton scheduler

---
+++ scripts/power_scheduler.py
@@ -0,0 +1,12 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+# Start the scheduler
+sched = BlockingScheduler()
+
+def scheduler_job():
+	print("Job executed")
+
+# Schedule the job to run once each minute
+sched.add_job(scheduler_job, 'cron', minute='0-59')
+sched.start()
+
